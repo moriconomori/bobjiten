c3b4fc490b55713be5ebe9b4865135fb88cfa532
Morico <moriconomori@gmail.com>
2019-10-05 01:35:18 +0900

    v1.1.0

--- app.js
+++ app.js
@@ -3,56 +3,68 @@ var app = new Vue({
   data: {
     word: "お題 CARD",
     words: [],
-    cardColor: ""
+    cardColor: '',
+    type: '',
   },
   methods: {
-    pickup: function (event) {
+    pickup: function(event) {
       var index = Math.floor(Math.random() * this.words.length);
       this.word = this.words[index];
+
       var whois = Math.floor(Math.random() * 100) + 1;
       if (whois <= 10) {
         this.cardColor = "rgb(255, 234, 42)";
+        this.type = 'カタコト';
       } else if (whois > 10 && whois <= 20) {
         this.cardColor = "rgb(129, 214, 116)";
+        this.type = 'ジェスチャー';
       } else {
         this.cardColor = "";
+        this.type = '';
       }
+
       var audio = document.getElementById('odai');
       audio.play();
-      gtag('event', 'click', {
-        'event_category': 'button',
-        'event_label': 'お題',
-        'value': 1
-      });
+      gtag('event', 'click', { 'event_category': 'button', 'event_label': 'お題' })
     },
-    correct: function (event) {
+    correct: function(event) {
       var audio = document.getElementById('correct');
       audio.play();
-      gtag('event', 'click', {
-        'event_category': 'button',
-        'event_label': '正解',
-        'value': 1
-      });
+      gtag('event', 'click', { 'event_category': 'button', 'event_label': '正解' });
     },
-    incorrect: function (event) {
+    incorrect: function(event) {
       var audio = document.getElementById('incorrect');
       audio.play();
-      gtag('event', 'click', {
-        'event_category': 'button',
-        'event_label': '不正解',
-        'value': 1
-      });
+      gtag('event', 'click', { 'event_category': 'button', 'event_label': '不正解' });
     },
   },
-  created: async function () {
+  created: async function() {
     var data = [];
     await axios.get('./data/bob_dictionary_1.txt')
-      .then(function (res) {
+      .then(function(res) {
         data = res.data.split(/\r\n|\r|\n/).filter(v => v);
       })
-      .catch(function (err) {
+      .catch(function(err) {
         console.log('Error: bob_dictionary_1.txt');
       });
-    this.words = data;
+    this.words = this.words.concat(data);
+
+    await axios.get('./data/bob_dictionary_2.txt')
+      .then(function(res) {
+        data = res.data.split(/\r\n|\r|\n/).filter(v => v);
+      })
+      .catch(function(err) {
+        console.log('Error: bob_dictionary_2.txt');
+      });
+    this.words = this.words.concat(data);
+
+    await axios.get('./data/my_dictionary.txt')
+      .then(function(res) {
+        data = res.data.split(/\r\n|\r|\n/).filter(v => v);
+      })
+      .catch(function(err) {
+        console.log('Error: my_dictionary.txt');
+      });
+    this.words = this.words.concat(data);
   }
 });
