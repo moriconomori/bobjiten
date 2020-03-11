851aa39199981fb9ad5fb01f373191671871f6a0
Morico <moriconomori@gmail.com>
2020-03-12 02:07:01 +0900

    vue add quasar

---
+++ src/quasar.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+
+import 'quasar/dist/quasar.css'
+import lang from 'quasar/lang/ja.js'
+import '@quasar/extras/material-icons/material-icons.css'
+import { Quasar } from 'quasar'
+
+Vue.use(Quasar, {
+  config: {},
+  components: {
+    /* not needed if importStrategy is not 'manual' */
+  },
+  directives: {
+    /* not needed if importStrategy is not 'manual' */
+  },
+  plugins: {},
+  lang: lang,
+})
