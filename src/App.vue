ec6315cf294bf79d6342febc97563bf9daaee5c5
Morico <moriconomori@gmail.com>
2020-03-12 16:40:59 +0900

    Home ページ作成

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,21 @@
 <template>
-  <div id="app">
-    <router-view />
-  </div>
+  <q-layout>
+    <q-page-container class="bg-light-blue-1">
+      <q-toolbar>
+        <q-toolbar-title class="text-center">
+          <span>カタヌキ</span>
+        </q-toolbar-title>
+      </q-toolbar>
+
+      <q-page class="q-px-xl">
+        <router-view />
+
+        <q-toolbar>
+          <q-toolbar-title class="text-center">
+            <small>&copy; 2019 Morico</small>
+          </q-toolbar-title>
+        </q-toolbar>
+      </q-page>
+    </q-page-container>
+  </q-layout>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,35 @@
 <template>
-  <div class="home">
-    Home
+  <div>
+    <q-responsive :ratio="16 / 9">
+      <q-card class="flex flex-center">
+        <q-card-section>
+          <span>カタヌキ</span>
+        </q-card-section>
+      </q-card>
+    </q-responsive>
+
+    <q-btn rounded size="16px" class="full-width q-py-xs">
+      <span>START</span>
+    </q-btn>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Home',
+
+  data: function() {
+    return {
+      words: {},
+    }
+  },
+
+  created: function() {},
 }
 </script>
+
+<style scoped>
+.q-btn--rounded {
+  border-radius: 999px;
+}
+</style>
