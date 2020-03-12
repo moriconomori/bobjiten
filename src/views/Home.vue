f760c7ae31b55b6b729e9fb9ec3521a5d1b729fc
Morico <moriconomori@gmail.com>
2020-03-12 18:16:42 +0900

    npm install axios vue-axios

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,20 @@
 <template>
   <div>
-    <q-responsive :ratio="16 / 9">
+    <q-responsive :ratio="16 / 9" class="q-mb-lg">
       <q-card class="flex flex-center">
         <q-card-section>
-          <span>カタヌキ</span>
+          <span>{{ card }}</span>
         </q-card-section>
       </q-card>
     </q-responsive>
 
-    <q-btn rounded size="16px" class="full-width q-py-xs">
+    <q-btn
+      rounded
+      color="primary"
+      size="16px"
+      :disable="loading"
+      class="full-width q-py-xs"
+    >
       <span>START</span>
     </q-btn>
   </div>
@@ -20,11 +26,27 @@ export default {
 
   data: function() {
     return {
+      loading: true,
       words: {},
+      card: 'Loading...',
     }
   },
 
-  created: function() {},
+  created: async function() {
+    this.words = (
+      await this.axios.get(
+        'https://script.google.com/macros/s/AKfycbws3-V2qvM3RY3rLlmtw_D0cmeYHWbf8xUJx_cnQ885x_Cs3cU/exec'
+      )
+    ).data
+    this.init()
+  },
+
+  methods: {
+    init() {
+      this.loading = false
+      this.card = 'お題 CARD'
+    },
+  },
 }
 </script>
 
