f9541f1352978beed5f1e3600db91286f005ad53
Morico <moriconomori@gmail.com>
2020-03-18 02:43:30 +0900

    footer を追加

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,15 +6,51 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import theme from '../theme';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary">
+      &copy; 2019 Morico
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    minHeight: '100vh',
+  },
+  footer: {
+    padding: theme.spacing(1, 0),
+    marginTop: 'auto',
+    backgroundColor:
+      theme.palette.type === 'light'
+        ? theme.palette.grey[200]
+        : theme.palette.grey[800],
+  },
+}));
 
 const Layout = ({ children }) => {
+  const classes = useStyles();
+
   return (
-    <React.Fragment>
+    <div className={classes.root}>
       <AppBar position="static">
-        <Toolbar>カタヌキ</Toolbar>
+        <Container maxWidth="sm" disableGutters>
+          <Toolbar>カタヌキ</Toolbar>
+        </Container>
       </AppBar>
-      <div>{children}</div>
-    </React.Fragment>
+      <main>{children}</main>
+      <footer className={classes.footer}>
+        <Container maxWidth="sm">
+          <Copyright />
+        </Container>
+      </footer>
+    </div>
   );
 };
 
