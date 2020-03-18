22c15d7ed0411ae83025ee807f783951dc3c1ad5
Morico <moriconomori@gmail.com>
2020-03-18 20:53:26 +0900

    index ページを作成

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,53 @@
 import React from 'react';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 import Box from '@material-ui/core/Box';
+import Link from 'next/link';
 
 export default function Index() {
   return (
     <Container maxWidth="sm" disableGutters>
-      <Box>
-        <Typography variant="h4" component="h1">
+      <Box my={4}>
+        <Typography variant="h2" component="h1" align="center">
           カタヌキ
         </Typography>
+      </Box>
+
+      <Box mb={8}>
+        <Typography align="center">
+          カタカナ言葉をカタカナ抜きで説明しよう！
+        </Typography>
+      </Box>
+
+      <Box my={2} px={8}>
+        <Box mb={4}>
+          <Link href="/settings">
+            <Button
+              variant="contained"
+              color="primary"
+              size="large"
+              fullWidth
+              style={{ borderRadius: 50 }}
+            >
+              設定
+            </Button>
+          </Link>
+        </Box>
+
+        <Box mb={2}>
+          <Link href="/play">
+            <Button
+              variant="contained"
+              color="primary"
+              size="large"
+              fullWidth
+              style={{ borderRadius: 50 }}
+            >
+              始める
+            </Button>
+          </Link>
+        </Box>
       </Box>
     </Container>
   );
