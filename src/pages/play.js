b07d266d39e0b6d8f873049a2ecd80933568d906
Morico <moriconomori@gmail.com>
2020-03-18 21:12:07 +0900

    /play と /settings ページを作成

---
+++ src/pages/play.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+
+export default function Play() {
+  return (
+    <Container maxWidth="sm">
+      <Typography variant="subtitle1">Play</Typography>
+    </Container>
+  );
+}
---
+++ src/pages/settings.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+
+export default function Settings() {
+  return (
+    <Container maxWidth="sm">
+      <Typography variant="subtitle1">Settings</Typography>
+    </Container>
+  );
+}
