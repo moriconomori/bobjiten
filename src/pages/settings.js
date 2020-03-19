df06c89d62ec5a4ee61fd76152797a0152a7968b
Morico <moriconomori@gmail.com>
2020-03-20 02:54:14 +0900

    propTypes を追加

--- src/pages/settings.js
+++ src/pages/settings.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import List from '@material-ui/core/List';
@@ -266,6 +267,11 @@ const Settings = ({ settings, setSettings }) => {
       </Box>
     </Container>
   );
+};
+
+Settings.propTypes = {
+  settings: PropTypes.object,
+  setSettings: PropTypes.function,
 };
 
 const mapStateToProps = state => ({
