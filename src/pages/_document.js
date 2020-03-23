e072f7ae0c1431c6e5b3a65022b99433527d4df6
Morico <moriconomori@gmail.com>
2020-03-23 20:36:31 +0900

    Roboto フォントの読み込みを削除

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -10,10 +10,6 @@ export default class MyDocument extends Document {
         <Head>
           {/* PWA primary color */}
           <meta name="theme-color" content={theme.palette.primary.main} />
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-          />
           <link rel="icon" type="image/x-icon" href="/favicon.ico" />
         </Head>
         <body>
